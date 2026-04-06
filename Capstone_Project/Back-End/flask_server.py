7f4fdb75f6290ad8b996cb8f80b4136c519e5263
IPochynyukCoding <157660033+IPochynyukCoding@users.noreply.github.com>
2026-04-06 16:32:44 -0500

    added customer deletion and linked employee to main flask server

--- Capstone_Project/Back-End/flask_server.py
+++ Capstone_Project/Back-End/flask_server.py
@@ -2,11 +2,13 @@ import flask
 import dotenv
 from os import environ
 from routes.customer import customer_blueprint
+from routes.employee import employee_blueprint
 from datetime import timedelta
 flask_server =flask.Flask(__name__)
 dotenv.load_dotenv("backend_access.env")
 flask_server.secret_key=environ['SECRET_KEY']
 flask_server.permanent_session_lifetime=timedelta(days=7)
 flask_server.register_blueprint(customer_blueprint)
+flask_server.register_blueprint(employee_blueprint)
 if __name__ =="__main__":
     flask_server.run(port=3000,debug=True)
