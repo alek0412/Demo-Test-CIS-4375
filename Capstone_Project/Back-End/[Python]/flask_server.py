c50d64e2af00abc7e4af03b023e264222b86be66
IPochynyukCoding <157660033+IPochynyukCoding@users.noreply.github.com>
2026-04-07 20:03:28 -0500

    Fixed port and added court to flask_server

--- Capstone_Project/Back-End/[Python]/flask_server.py
+++ Capstone_Project/Back-End/[Python]/flask_server.py
@@ -4,14 +4,16 @@ import waitress
 from os import environ
 from routes.customer import customer_blueprint
 from routes.employee import employee_blueprint
+from routes.court import court_blueprint
 from datetime import timedelta
 flask_server =flask.Flask(__name__)
 dotenv.load_dotenv("backend_access.env")
 flask_server.secret_key=environ['SECRET_KEY']
 flask_server.permanent_session_lifetime=timedelta(days=7)
 flask_server.register_blueprint(customer_blueprint)
-# flask_server.register_blueprint(employee_blueprint)
+flask_server.register_blueprint(employee_blueprint)
+flask_server.register_blueprint(court_blueprint)
 if __name__ =="__main__":
-    waitress.serve(flask_server,port=3000)
+    waitress.serve(flask_server,port=3001)
     
     
