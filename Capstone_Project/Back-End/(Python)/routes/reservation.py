cbf213c5005abebd60f19de00d34ba091fc47714
IPochynyukCoding <157660033+IPochynyukCoding@users.noreply.github.com>
2026-04-11 01:30:52 -0500

    Fixed bug for denying customer

--- Capstone_Project/Back-End/(Python)/routes/reservation.py
+++ Capstone_Project/Back-End/(Python)/routes/reservation.py
@@ -175,22 +175,10 @@ def reservation_approval():
         )
         if type(update_customer) == int:
             return make_response("Server is unable to update customer", 503)
-    elif reservation_status == 4:
-        # Match Node admin cancel: waiver_status 2 = eligible to book (see add_reservation check).
-        cid = reservation_fetch[0]["customer_id"]
-        still_active = sql_functions.execute_read(
-            secure_connection,
-            "select count(*) as c from reservation where customer_id=%s and reservation_status in (1,2)",
-            (cid,),
-        )
-        if type(still_active) != int and still_active and int(still_active[0]["c"]) == 0:
-            update_customer = sql_functions.execute_query(
-                secure_connection,
-                "update waiver set waiver_status=2 where customer_id=%s",
-                (cid,),
-            )
-            if type(update_customer) == int:
-                return make_response("Server is unable to update customer", 503)
+    elif reservation_status == 3:
+        update_customer = sql_functions.execute_query(secure_connection,"update waiver set waiver_status=1 where customer_id=%s",(reservation_fetch[0]["customer_id"],))
+        if type(update_customer) == int:
+            return make_response("Server is unable to update customer", 503)
     if current == 1:
         verb = "approved" if reservation_status == 2 else "denied"
     else:
